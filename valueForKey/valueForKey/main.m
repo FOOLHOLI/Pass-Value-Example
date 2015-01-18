5eeaef4be9a490fd34c45de5e84659983ca3c815
Su, Jian-Hau <jenhausu@icloud.com>
2015-01-18 15:02:10 +0800

    專案初始化

---
+++ valueForKey/valueForKey/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  valueForKey
+//
+//  Created by 蘇健豪1 on 2015/1/18.
+//  Copyright (c) 2015年 蘇健豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
