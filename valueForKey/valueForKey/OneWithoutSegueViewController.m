8067e74616aa907ab3615612b4f98394fd1051bb
Su, Jian-Hau <jenhausu@icloud.com>
2015-01-18 16:38:01 +0800

    傳值過去

--- valueForKey/valueForKey/OneWithoutSegueViewController.m
+++ valueForKey/valueForKey/OneWithoutSegueViewController.m
@@ -10,28 +10,24 @@
 
 @interface OneWithoutSegueViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (strong, nonatomic) UIViewController *page2;
+
 @end
 
 @implementation OneWithoutSegueViewController
 
+@synthesize page2;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    page2 = [self.storyboard instantiateViewControllerWithIdentifier:@"page2"];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)buttonPress:(id)sender {
+    [page2 setValue:self.textField.text forKey:@"receiveData"];
+    [self.navigationController pushViewController:page2 animated:YES];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
--- valueForKey/valueForKey/TwoWithoutSegueViewController.m
+++ valueForKey/valueForKey/TwoWithoutSegueViewController.m
@@ -10,28 +10,19 @@
 
 @interface TwoWithoutSegueViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (strong, nonatomic) NSString *receiveData;
+
 @end
 
 @implementation TwoWithoutSegueViewController
 
+@synthesize receiveData;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.textField.text = receiveData;
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
